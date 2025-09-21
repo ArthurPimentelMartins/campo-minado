207f27a071d8f447eb77ff5034594ea414aa2a7e
ArthurPimentelMartins <arthurpmartins2006@gmail.com>
2025-09-20 22:40:50 -0300

    commit and push

---
+++ Test/br/com/cod3r/cm/modelo/CampoTest.java
@@ -0,0 +1,120 @@
+package br.com.cod3r.cm.modelo;
+
+import br.com.cod3r.cm.excecao.ExplosaoException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CampoTest {
+
+    private Campo campo;
+
+    @BeforeEach
+    void iniciarCampo() {
+        campo = new Campo(3, 3);
+    }
+
+    @Test
+    void testeVizinhoRealDistancia1() {
+
+        Campo vizinhoEsquerda = new Campo(3, 2);
+        boolean resultadoEsquerda = campo.adicionarVizinho(vizinhoEsquerda);
+        assertTrue(resultadoEsquerda);
+
+        Campo vizinhoDireita = new Campo(3, 4);
+        boolean resultadoDireita = campo.adicionarVizinho(vizinhoDireita);
+        assertTrue(resultadoDireita);
+
+        Campo vizinhoCima = new Campo(2, 3);
+        boolean resultadoCima = campo.adicionarVizinho(vizinhoCima);
+        assertTrue(resultadoCima);
+
+        Campo vizinhoBaixo = new Campo(4, 3);
+        boolean resultadoBaixo = campo.adicionarVizinho(vizinhoBaixo);
+        assertTrue(resultadoBaixo);
+    }
+
+    @Test
+    void testeVizinhoRealDistancia2Diagonal() {
+        Campo vizinho = new Campo(2, 2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    void testeNaoVizinhoRealDistancia2Diagonal() {
+        Campo vizinho = new Campo(1, 2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertFalse(resultado);
+    }
+
+    @Test
+    void testeValorPadraoAtributoMarcado() {
+        assertFalse(campo.isMarcado());
+    }
+
+    @Test
+    void testeAlternarMarcacaoDuasChamadas() {
+        campo.alternarMarcacao();
+        campo.alternarMarcacao();
+        assertFalse(campo.isMarcado());
+    }
+
+    @Test
+    void testeAbrirNaoMinadoNaoMarcado() {
+        assertTrue(campo.abrir());
+    }
+
+    @Test
+    void testeAbrirNaoMinadoMarcado() {
+        campo.alternarMarcacao();
+        assertFalse(campo.abrir());
+    }
+
+    @Test
+    void testeAbrirMinadoMarcado() {
+        campo.alternarMarcacao();
+        campo.minar();
+        assertFalse(campo.abrir());
+    }
+
+    @Test
+    void testeAbrirMinadoNaoMarcado() {
+        campo.minar();
+
+        assertThrows(ExplosaoException.class, () -> {
+            campo.abrir();
+        });
+    }
+
+    @Test
+    void testeAbrirComVizinhos1() {
+
+        Campo campo11 = new Campo(1, 1);
+
+        Campo campo22 = new Campo(2, 2);
+        campo22.adicionarVizinho(campo11);
+
+        campo.adicionarVizinho(campo22);
+        campo.abrir();
+        assertTrue(campo22.isAberto() && campo11.isAberto());
+    }
+
+    @Test
+    void testeAbrirComVizinhos2() {
+
+        Campo campo11 = new Campo(1, 1);
+        Campo campo12 = new Campo(1, 1);
+        campo12.minar();
+
+        Campo campo22 = new Campo(2, 2);
+        campo22.adicionarVizinho(campo11);
+        campo22.adicionarVizinho(campo12);
+
+        campo.adicionarVizinho(campo22);
+        campo.abrir();
+
+        assertFalse(campo12.isAberto() && !campo11.isAberto());
+    }
+}
---
+++ src/br/com/cod3r/cm/visao/TabuleiroConsole.java
@@ -0,0 +1,79 @@
+package br.com.cod3r.cm.visao;
+
+import br.com.cod3r.cm.excecao.ExplosaoException;
+import br.com.cod3r.cm.excecao.SairException;
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class TabuleiroConsole {
+
+    private final Tabuleiro tabuleiro;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public TabuleiroConsole(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+    }
+
+    private void executarJogo() {
+        try {
+            boolean continuar = true;
+
+            while (continuar) {
+                cicloDoJogo();
+
+                System.out.println("Outra partida? (S/n) ");
+                String resposta = scanner.nextLine();
+
+                if ("n".equalsIgnoreCase(resposta)) {
+                    continuar = false;
+                } else {
+                    tabuleiro.reiniciar();
+                }
+            }
+
+        } catch (SairException e) {
+            System.out.println("Tchau!!");
+        } finally {
+            scanner.close();
+        }
+    }
+
+    private void cicloDoJogo() {
+
+        try {
+
+            while (!tabuleiro.objetivoAlcancado()) {
+                System.out.println(tabuleiro);
+
+                String digitado = capturarValorDigitado("Digite (x, y): ");
+
+                Iterator<Integer> xy = Arrays.stream(digitado.split(",")).map(e -> Integer.parseInt(e.trim())).iterator();
+
+                digitado = capturarValorDigitado("1 - Abrir ou 2 - (Des)Marcar");
+
+                if ("1".equals(digitado)) {
+                    tabuleiro.abrir(xy.next(), xy.next());
+                } else if ("2".equals(digitado)) {
+                    tabuleiro.alternarMarcacao(xy.next(), xy.next());
+                }
+            }
+
+            System.out.println("Você ganhou!!");
+        } catch (ExplosaoException e) {
+            System.out.println("Você perdeu!");
+        }
+    }
+
+    private String capturarValorDigitado(String texto) {
+        System.out.print(texto);
+        String digitado = scanner.nextLine();
+
+        if ("sair".equalsIgnoreCase(digitado)) {
+            throw new SairException();
+        }
+        return digitado;
+    }
+}
